828c770d9083ce6b60aa82c2f1d5abe7b1fe4d86
fangxin-tang <susantang.nz@gmail.com>
2023-11-02 14:33:43 +1300

    Display movie list

--- src/c_view.py
+++ src/c_view.py
@@ -9,17 +9,13 @@ from a_models import *
 
 ############################ Get Data From Files ############################
 def load_movie_names_from_file():
-    try:
-        with open('src/db/movie.txt', mode='r') as file:
-            movie_lines = file.readlines()
-            movie_names=[]
-            # Create a list of movie name list
-            for line in movie_lines:
-                movie_names.append(line[0])
-        return movie_names
-    except FileNotFoundError:
-        print('Movie File not found')
-
+    movie_names=[]
+    with open('src/db/movie.txt', mode='r') as file:
+        for line in file:
+            line = line.strip().split('|')
+            name=line[0]
+            movie_names.append(name)
+    return movie_names
 
 
 ############################ Click Btn Functions ############################
@@ -30,16 +26,21 @@ def btn_show_movie_list():
     # populate data into listbox
     for name in movie_names:
         movie_listbox.insert(tk.END, name)
+    print("clicked")
 
-####### GUI #######
+############################ Test ############################
+
+
+
+############################ GUI ############################
 # Create a root
 root = tk.Tk() 
 root.title("Movie Ticket Booking System")
 root.geometry("920x800+30+30")
 root.config(padx=10, pady=10)
 
-
-# # Create a clinic logo
+############## Heading #################
+## Create a cinema logo
 image_path = os.path.join(os.getcwd())+'/src/logo.png' ## Specify the full path to the image file
 print(f"image_path = {image_path}")
 img = Image.open(image_path) ## open the image
@@ -61,7 +62,6 @@ heading_label.grid(row=0,column=1,pady=10)
 ############# Section One: movie list, movie details, screenings  #############
 
 ###### movie list box #########
-
 ## Create a title label
 movie_lst_title_label = ttk.Label(root,text='Movie List',font=("Helvetica", 16))
 movie_lst_title_label.grid(row=1,column=0, sticky='w',pady=5, padx=(30,0))
@@ -71,16 +71,18 @@ movie_listbox = tk.Listbox(root, width=20,height=15, exportselection=0, selectmo
 movie_listbox.grid(row=2,column=0,padx=(30,0))
 
 ## Create a button to show all movies
-button = ttk.Button(root,text="Show Movie List",command="btn_show_movie_list")
+button = ttk.Button(root,text="Show Movie List",command=btn_show_movie_list)
 button.grid(row=3,column=0,sticky='nw',pady=5, padx=(30,0))
 
 
+###### movie description text_widget #########
+# movie description text_widget:
+## Create a title label
+description_title_label = ttk.Label(root,text='Description',font=("Helvetica", 16))
+description_title_label.grid(row=1,column=1, sticky='w',pady=5,padx=(50,0))
 
-# # Doctor list:
-# ## Create a title label
-# doctor_lst_title_label = ttk.Label(root,text='Doctor List',font=("Helvetica", 16))
-# doctor_lst_title_label.grid(row=1,column=1, sticky='w',pady=5,padx=(50,0))
-
+text_widget = scrolledtext.ScrolledText(root, wrap=tk.WORD,width=15, height=20)
+text_widget.grid(row=2, column=1,padx=(25,0),sticky='ew')
 # ## Create a listbox for displaying doctors
 # doctor_listbox = tk.Listbox(root, width=30,height=15,exportselection=0,selectmode=tk.SINGLE)
 # doctor_listbox.grid(row=2,column=1, sticky='w',padx=(50, 0)) 
@@ -91,5 +93,11 @@ button.grid(row=3,column=0,sticky='nw',pady=5, padx=(30,0))
 
 
 
-
+#exit button
+exit_button = ttk.Button(
+    root,
+    text='Exit',
+    command=lambda: root.quit()
+)
+exit_button.grid(row=9,column=2,sticky='es',pady=(30,0))
 root.mainloop()
