a6f24bfc0b2dc86085964538a396d9255028cdb9
FangxinTang <130117000+FangxinTang@users.noreply.github.com>
2023-12-18 21:53:00 +1300

    Update users

--- backend/src/users/models.py
+++ backend/src/users/models.py
@@ -1,31 +1,33 @@
 """Database ORM models"""
+from typing_extensions import Annotated
+# import sys
+# import pathlib as pl
+# sys.path.insert(0, str(pl.Path(__file__).resolve().parent.parent))
+from ..models import Person
 from sqlalchemy import orm
-from ..persons import models as person_models
 
+required_field = Annotated[str, orm.mapped_column(nullable=False, unique=True)]
 
-required_field = orm.mapped_column(nullable=False, unique=True)
-
-class User(person_models):
-    """Abstract class User"""
+class User(Person):
     __tablename__ = "users"
 
-    username: orm.Mapped[str] = required_field
-    password: orm.Mapped[str] = orm.mapped_column(nullable=False)
+    username: orm.Mapped[required_field] 
+    password: orm.Mapped[required_field]
 
 
-# ==== DUMMY DATA ==== #
+# # ==== DUMMY DATA ==== #
 
-DUMMY_DATA = [
-    (
-        User,
-        {
-            "username": "sam_wang",
-            "password": "secretpass"
-        },
-    )
-]
+# DUMMY_DATA = [
+#     (
+#         User,
+#         {
+#             "username": "sam_wang",
+#             "password": "secretpass"
+#         },
+#     )
+# ]
 
 
-# ==== REGISTER ==== #
+# # ==== REGISTER ==== #
 
-MODELS_REGISTER = [User]
+# MODELS_REGISTER = [User]
