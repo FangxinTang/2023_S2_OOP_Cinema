c364adb1446408272ead0b823c0e7f1d2761f25b
FangxinTang <130117000+FangxinTang@users.noreply.github.com>
2023-12-19 18:15:39 +1300

    update booking class

--- backend/src/models/booking.py
+++ backend/src/models/booking.py
@@ -1,24 +1,11 @@
 """Create ShowTime model"""
-import datetime as dt
 from sqlalchemy.orm import Mapped, mapped_column
 from database.db_init import BaseModel
 
 
-class ShowTime(BaseModel):
-    __tablename__ = "showtimes"
+class Booking(BaseModel):
+    __tablename__ = "bookings"
 
-    show_date: Mapped[dt.datetime] = mapped_column(
-        unique=False,
-        default=dt.datetime(2000, 10, 10)
-    )
-
-    start_time: Mapped[dt.datetime] = mapped_column(
-        nullable=True,
-        default=dt.datetime(10, 30)
-    )
-
-    end_time: Mapped[dt.datetime] = mapped_column(
-        nullable=True,
-        default=dt.datetime(12, 30)
-    )
-
+    num_seats: Mapped[int] = mapped_column(nullable=False)
+    status: Mapped[int] = mapped_column(nullable=False, default=1)
+    order_total: Mapped[float] = mapped_column(nullable=False)
