63e085b60399b811b4b4e928555e26ae22e68ec0
FangxinTang <130117000+FangxinTang@users.noreply.github.com>
2023-12-27 15:27:24 +1300

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -3,14 +3,18 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
+import os
+import sys
+print("cwd: ", os.getcwd())
+print('Python Path', sys.path)
+root_directory = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+backend_src_path = os.path.join(root_directory, 'backend', 'src')
+sys.path.append(backend_src_path)
 from database.models import Base
 
 from alembic import context
 
-# import os
-# import sys
-# print("cwd: ", os.getcwd())
-# print('Python Path', sys.path)
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
